594b8520c17a4b2bb75075cf0e4fd5b668bf0c5e
ankitp700706 <ankit.p700706@gmail.com>
2021-10-16 12:10:52 +0530

    Login/Logout implemented

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, View, FlatList } from "react-native";
 
 import { ListItem, ListItemSeparator } from "../components/lists";
@@ -6,6 +6,7 @@ import routes from "../navigation/routes";
 import colors from "../config/colors";
 import Icon from "../components/Icon";
 import Screen from "../components/Screen";
+import AuthContext from "../auth/context";
 
 const menuItems = [
 	{
@@ -26,12 +27,13 @@ const menuItems = [
 ];
 
 function AccountScreen({ navigation }) {
+	const { user, setUser } = useContext(AuthContext);
 	return (
 		<Screen style={styles.screen}>
 			<View style={styles.container}>
 				<ListItem
-					title="Mosh Hamedani"
-					subTitle="programmingwithmosh@gmail.com"
+					title={user.name}
+					subTitle={user.email}
 					image={require("../assets/profile.jpg")}
 				/>
 			</View>
@@ -57,6 +59,7 @@ function AccountScreen({ navigation }) {
 			<ListItem
 				title="Log Out"
 				IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+				onPress={() => setUser(null)}
 			/>
 		</Screen>
 	);
